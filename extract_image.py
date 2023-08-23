450bbaed60081db69c4716cdf28a7abde005d667
Cosmo Chiou <chioujryu@gmail.com>
2023-08-23 17:14:35 +0800

    2023.08.23.17.14

--- extract_image.py
+++ extract_image.py
@@ -87,10 +87,10 @@ def show_image(image, format):
         plt.imshow(image)                                    # 在圖表中繪製圖片    
         plt.show() 
 
-def convert_pdf_to_jpg(pdf_path:str, poppler_path:str, save_jpg_path:str) -> np.ndarray:
+def convert_pdf_to_jpg_windows_os(pdf_path:str, poppler_path:str, save_jpg_path:str) -> np.ndarray:
     '''
     Description:
-        將 pdf 轉換成圖片
+        將 pdf 轉換成圖片，這個function只能給 windows 作業系統用
     
     Parameters:
         pdf_path (str): pdf的路徑. 
@@ -110,6 +110,43 @@ def convert_pdf_to_jpg(pdf_path:str, poppler_path:str, save_jpg_path:str) -> np.
 
     # Store Pdf with convert_from_path function
     images = convert_from_path(pdf_path, poppler_path = poppler_path)   # r'poppler-23.08.0/Library/bin'
+    
+    # how many image
+    image_number = len(images)
+
+    for i in range(image_number):
+        # Save pages as images in the pdf
+        images[i].save(save_jpg_path + '/' + 'page'+ str(i) +'.jpg', 'JPEG')
+
+    # 將 PIL 圖像物件轉換成 NumPy 陣列
+    image_array = []
+    for i in range(image_number):
+        image_array.append(np.array(images[i]))  
+
+    return image_array
+
+def convert_pdf_to_jpg_linux_os(pdf_path:str, save_jpg_path:str) -> np.ndarray:
+    '''
+    Description:
+        將 pdf 轉換成圖片，這個function只能給 windows 作業系統用
+    
+    Parameters:
+        pdf_path (str): pdf的路徑. 
+        
+    Returns:
+        np.ndarray: (幾張圖片, 圖片的寬, 圖片的高, 圖片的通道數)
+        
+    Examples:
+        >>> image_array = convert_pdf_to_jpg('data/pdf/Auxiliary Tasks in Multi-task Learning.pdf', 'poppler-23.08.0/Library/bin')
+        >>> print(image_array.shape)
+        (8, 2200, 1700, 3)
+
+    Exception Handling:
+        Raises a ValueError if the provided str is empty.
+    '''
+
+    # Store Pdf with convert_from_path function
+    images = convert_from_path(pdf_path)   # r'poppler-23.08.0/Library/bin'
     
     # how many image
     image_number = len(images)
--- test-for-development.py
+++ test-for-development.py
@@ -4,74 +4,33 @@ import matplotlib.image as img
 import os
 import numpy as np
 from pdf2image import convert_from_path # 如果要使用 convert_from_path 的話，需要下載 poppler 到同個資料夾
-from extract_image import convert_pdf_to_jpg, show_image
+from extract_image import show_image,convert_pdf_to_jpg_linux_os
 import cv2
 import keras_ocr
 import math
+from inpaint_text import *
 
 
-image_array = convert_pdf_to_jpg('data/pdf/1_pic.pdf', 'poppler-23.08.0/Library/bin', 'data/jpg')
+images = convert_pdf_to_jpg_linux_os('/opt/cosmo_home/side_project/extract-image-from-jpg-or-pdf/data/pdf/1_pic.pdf',
+                                     '/opt/cosmo_home/side_project/extract-image-from-jpg-or-pdf/data/jpg')
+
+
 
 # convert BGR to GRAY
-gray = cv2.cvtColor(image_array[0], cv2.COLOR_BGR2GRAY)    # cv2.COLOR_BGR2GRAY
+gray = cv2.cvtColor(images[0], cv2.COLOR_BGR2GRAY)    # cv2.COLOR_BGR2GRAY
 ret, output1 = cv2.threshold(gray, 180, 255, cv2.THRESH_BINARY)     # 如果大於 127 就等於 255，反之等於 0。
 
+show_image(output1, 'rgb')
 
-#General Approach.....
-#Use keras OCR to detect text, define a mask around the text, and inpaint the
-#masked regions to remove the text.
-#To apply the mask we need to provide the coordinates of the starting and 
-#the ending points of the line, and the thickness of the line
-
-#The start point will be the mid-point between the top-left corner and 
-#the bottom-left corner of the box. 
-#the end point will be the mid-point between the top-right corner and the bottom-right corner.
-#The following function does exactly that.
-def midpoint(x1, y1, x2, y2):
-    x_mid = int((x1 + x2)/2)
-    y_mid = int((y1 + y2)/2)
-    return (x_mid, y_mid)
-
-#Main function that detects text and inpaints. 
-#Inputs are the image path and kreas_ocr pipeline
-def inpaint_text(img_path, pipeline):
-    # read the image 
-    img = keras_ocr.tools.read(img_path) 
-    
-    # Recogize text (and corresponding regions)
-    # Each list of predictions in prediction_groups is a list of
-    # (word, box) tuples. 
-    prediction_groups = pipeline.recognize([img])
-    
-    #Define the mask for inpainting
-    mask = np.zeros(img.shape[:2], dtype="uint8")
-    for box in prediction_groups[0]:
-        x0, y0 = box[1][0]
-        x1, y1 = box[1][1] 
-        x2, y2 = box[1][2]
-        x3, y3 = box[1][3] 
-        
-        x_mid0, y_mid0 = midpoint(x1, y1, x2, y2)
-        x_mid1, y_mi1 = midpoint(x0, y0, x3, y3)
-        
-        #For the line thickness, we will calculate the length of the line between 
-        #the top-left corner and the bottom-left corner.
-        thickness = int(math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 ))
-        
-        #Define the line and inpaint
-        cv2.line(mask, (x_mid0, y_mid0), (x_mid1, y_mi1), 255,    
-        thickness)
-        inpainted_img = cv2.inpaint(img, mask, 7, cv2.INPAINT_NS)
-                 
-    return(inpainted_img)
-
-# keras-ocr will automatically download pretrained
 # weights for the detector and recognizer.
 pipeline = keras_ocr.pipeline.Pipeline()
 
+
+
+img = keras_ocr.tools.read(output1) 
+prediction_groups = pipeline.recognize([img])
+
 #img_text_removed = inpaint_text('traffic-signs.jpg', pipeline)
-img_text_removed = inpaint_text("data/jpg/page0.jpg", pipeline)
+#img_text_removed = inpaint_text(img = img, 
+#                                pipeline = pipeline)
 
-plt.imshow(img_text_removed)
-
-cv2.imwrite('text_removed_image.jpg', cv2.cvtColor(img_text_removed, cv2.COLOR_BGR2RGB))
